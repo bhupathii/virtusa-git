6d9af0dcabd4128b63f490d3108a880512aa3ec4
Bhupathi Reddy <vtu23091@veltech.edu.in>
2026-02-19 12:15:14 +0530

    Implement SBAccount with interest calculation
    Extend SBAccount from Account, add accountType field and a constructor that validates the initial amount (throws InvalidInputException for negative values). Import the exception, set the amount in the constructor, and override calculateInterest() to apply 4% for "Normal" accounts and 6% for "NRI" accounts, returning the computed interest.

--- Week-2/Day-9_10/InterestCalculator/src/com/interest/bean/SBAccount.java
+++ Week-2/Day-9_10/InterestCalculator/src/com/interest/bean/SBAccount.java
@@ -1,5 +1,20 @@
 package com.interest.bean;
-
-public class SBAccount {
-    
+import com.interest.exception.InvalidInputException;
+public class SBAccount extends Account {
+    private String accountType; 
+    public SBAccount(double amount, String accountType) throws InvalidInputException {
+        if (amount < 0)
+            throw new InvalidInputException("Invalid amount. Please enter non-negative values.");
+        this.amount = amount;
+        this.accountType = accountType;
+    }
+    @Override
+    public double calculateInterest() {
+        if (accountType.equalsIgnoreCase("Normal")) {
+            interestRate = 4.0;
+        } else if (accountType.equalsIgnoreCase("NRI")) {
+            interestRate = 6.0;
+        }
+        return (amount * interestRate) / 100;
+    }
 }
