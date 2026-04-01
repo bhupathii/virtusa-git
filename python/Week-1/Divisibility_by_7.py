707104280a9d1e697944eab170c75e4970dcef0d
Bhupathi Reddy <vtu23091@veltech.edu.in>
2026-04-01 15:09:45 +0530

    Add Week-1: divisibility and next power scripts
    Add two beginner Python solutions under python/Week-1: Divisibility_by_7.py reads an integer from stdin and prints "yes" if divisible by 7 otherwise "no"; Next_greater_power_of_2.py reads an integer and prints the smallest power of two strictly greater than the input.

---
+++ python/Week-1/Divisibility_by_7.py
@@ -0,0 +1,5 @@
+userInput = int(input())
+if userInput%7==0:
+    print("yes")
+else:
+    print("no")
---
+++ python/Week-1/Next_greater_power_of_2.py
@@ -0,0 +1,5 @@
+userInput = int(input())
+ans=1
+while ans<=userInput:
+    ans*=2
+print(ans)
